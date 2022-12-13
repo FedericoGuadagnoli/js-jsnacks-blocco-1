45ce03435593c51f1b66342006c6cc17d8aae14c
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2022-12-13 18:04:41 +0100

    Fine secondo esercizio

--- js/esercizio-2.js
+++ js/esercizio-2.js
@@ -7,10 +7,38 @@ console.log('JS OK');
 // Prendo gli elementi dalla pagina
 const input = document.getElementById('name');
 const button = document.getElementById('button');
-const message = document.getElementById('message');
+const target = document.getElementById('message');
 
 // Creo l'array con la lista dei nomi
 const list = ['marco', 'giulio', 'federico','simone'];
 
-// Prendo il value dell'input
-const userName = input.value;
+
+
+// Creo la funzione al click del bottone con la callback
+button.addEventListener('click', function(){
+
+    // Prendo il value dell'input
+    let userName = input.value.trim();
+
+    // Validazione
+    if (!userName || !isNaN(userName)) {
+        target.innerText = 'Devi inserire solo testo!'
+        return;
+    }
+
+    // Preparo una variabile booleana
+    let Isvalid = false;
+
+    // Creo il ciclo per la verifica del nome
+    for ( i=0; i < list.length; i++){
+        const validName = list[i];
+        
+        // Verifico se il nome dato dall'utente sia nella lista
+        if (validName === userName ) {
+            Isvalid = true; 
+        }
+    }
+    // Stabilisco cosa stampare in pagina
+    target.innerText = Isvalid ? 'Sei invitato alla festa' : 'Non sei invitato alla festa';
+
+});
